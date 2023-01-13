3d6e54d5cfe5d9035970491d0d05e4b61fd58bfd
hamadatomoki <hamada.tomoki01@gmail.com>
2023-01-13 14:09:24 +0900

    :books: Learn live search in phoenix

--- lv_ex/lib/lv_ex_web/live/search_live.ex
+++ lv_ex/lib/lv_ex_web/live/search_live.ex
@@ -1,26 +1,44 @@
 defmodule LvExWeb.SearchLive do
   use LvExWeb, :live_view
 
-  alias LiveViewStudio.Stores
+  alias LvEx.Stores
 
   def render(assigns) do
     ~H"""
-    <h1></h1>
+    <h1>Find Store</h1>
     <div id="search">
+      <form phx-submit="zip-search">
+        <input
+          type="text"
+          name="zip"
+          value={@zip}
+          placefolder="Zip Code"
+          autofocus
+          autocomplete="off"
+          readonly={@loading}
+        />
+        <button type="submit">
+          <img src="images/search.svg" alt="search" />
+        </button>
+      </form>
+      <div :if={@loading} class="loader">Loading...</div>
       <div class="stores">
         <ul>
-          <li>
+          <li :for={store <- @stores}>
             <div class="first-line">
-              <div class="name"></div>
+              <div class="name"><%= store.name %></div>
               <div class="status">
-                <span class="open"></span><span class="closed"></span>
+                <span :if={store.open} class="open"></span>
+                <span :if={!store.open} class="closed"></span>
               </div>
               <div class="second-line">
                 <div class="street">
                   <img src="images/location.svg" alt="location" />
+                  <%= store.street %>
                 </div>
                 <div class="phone_number">
                   <img src="images/phone.svg" alt="phone" />
+                  <%= store.phone_number %>
                 </div>
               </div>
             </div>
@@ -28,9 +46,6 @@ defmodule LvExWeb.SearchLive do
         </ul>
       </div>
     </div>
-    <section>
-      <h1><b><%= @user.name %></b></h1>
-    </section>
     """
   end
 
@@ -39,9 +54,41 @@ defmodule LvExWeb.SearchLive do
       assign(
         socket,
         zip: "",
-        stores: Stores.list_stores()
+        stores: Stores.list_stores(),
+        loading: false
       )
 
     {:ok, socket}
+  end
+
+  def handle_event("zip-search", %{"zip" => zip}, socket) do
+    send(self(), {:run_zip_search, zip})
+
+    socket =
+      assign(
+        socket,
+        zip: zip,
+        stores: [],
+        loading: true
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_info({:run_zip_search, zip}, socket) do
+    socket =
+      case Stores.search_by_zip(zip) do
+        [] ->
+          socket
+          |> put_flash(:info, "No store matching \"#{zip}\"")
+          |> assign(stores: [], loading: false)
+
+        stores ->
+          socket
+          |> clear_flash()
+          |> assign(socket, stores: stores, loading: false)
+      end
+
+    {:noreply, socket}
   end
 end
