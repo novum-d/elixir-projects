4f8be34f9551130d53b3d06a2d9d61278fa62619
novum-d <hamada.tomoki01@gmail.com>
2023-04-29 23:02:14 +0900

    :building_construction: chore: add graphql example

---
+++ pragstudio-unpacked-graphql-code/getaways-pagination/frontend/src/pages/Home.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import Error from "../components/Error";
+import Loading from "../components/Loading";
+import PlaceList from "../components/PlaceList";
+
+const GET_PLACES_QUERY = gql`
+  query GetPlaces($limit: Int!, $offset: Int) {
+    places(limit: $limit, offset: $offset) {
+      id
+      slug
+      name
+      location
+      description
+      pricePerNight
+      imageThumbnail
+      maxGuests
+    }
+  }
+`;
+
+const LIMIT = 3;
+
+class Home extends Component {
+  state = {
+    hasMorePlaces: true
+  };
+
+  render() {
+    return (
+      <Query query={GET_PLACES_QUERY} variables={{ limit: LIMIT, offset: 0 }}>
+        {({ data, loading, error, fetchMore }) => {
+          if (loading) return <Loading />;
+          if (error) return <Error error={error} />;
+          return (
+            <>
+              <PlaceList places={data.places} />
+              <div className="more">
+                <button
+                  disabled={!this.state.hasMorePlaces}
+                  onClick={() =>
+                    fetchMore({
+                      variables: {
+                        offset: data.places.length
+                      },
+                      updateQuery: (previousResult, { fetchMoreResult }) => {
+                        if (!fetchMoreResult) return previousResult;
+
+                        if (fetchMoreResult.places.length < LIMIT) {
+                          this.setState({ hasMorePlaces: false });
+                        }
+
+                        return {
+                          places: [
+                            ...previousResult.places,
+                            ...fetchMoreResult.places
+                          ]
+                        };
+                      }
+                    })
+                  }>
+                  See More
+                </button>
+              </div>
+            </>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default Home;
